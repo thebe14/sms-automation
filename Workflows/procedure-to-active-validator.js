d785636f73aa82f9912a06327c37a4f11242cec5
Levente F <thebe14@gmail.com>
2025-08-22 17:05:21 +0200

    Replaced conditions with validators for  Policy and Procedure tickets transitioning to Active
    This way, the transition can be attempted and an error message is shown if conditions not met
    If the field "Review frequency" is anything but "Together with process review", then the field "Next review" must not be empty

--- Workflows/procedure-to-active-validator.js
+++ Workflows/procedure-to-active-validator.js
@@ -1,3 +1,4 @@
+// Validation message: When the review frequency is not "Together with process review" you must specify the date of the next review
 // customfield_10096 is Next review
 // customfield_10389 is Review frequency
 null != issue.customfield_10096?.value || issue.customfield_10389?.value == "Together with process review"
