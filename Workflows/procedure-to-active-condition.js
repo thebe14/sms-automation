613c3e5f78334553160eb75111aee97b7a705f95
Levente F <thebe14@gmail.com>
2025-08-22 16:54:59 +0200

    Added conditions to Policy and Procedure tickets for transitioning to Active
    If the field "Review frequency" is anything but "Together with process review", then the field "Next review" must not be empty

---
+++ Workflows/procedure-to-active-condition.js
@@ -0,0 +1,3 @@
+// customfield_10096 is Next review
+// customfield_10389 is Review frequency
+null != issue.customfield_10096?.value || issue.customfield_10389?.value == "Together with process review"
