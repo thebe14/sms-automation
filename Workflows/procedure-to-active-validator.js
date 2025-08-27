f86789f51019d2891cb2a6fa7a18e857f432329c
Levente F <thebe14@gmail.com>
2025-08-28 01:02:50 +0200

    Fixed validators to transition Policy or Procedure to Active

--- Workflows/procedure-to-active-validator.js
+++ Workflows/procedure-to-active-validator.js
@@ -1,4 +1,5 @@
 // Validation message: When the review frequency is not "Together with process review" you must specify the date of the next review
 // customfield_10096 is Next review
 // customfield_10389 is Review frequency
-null != issue.customfield_10096?.value || issue.customfield_10389?.value == "Together with process review"
+issue.customfield_10389?.value == "Together with process review" || 
+(null != issue.customfield_10389?.value && null != issue.customfield_10096)
