38c02f99af113e6eedfd89e3aab3fcbef6e5d86b
Levente F <thebe14@gmail.com>
2025-08-28 14:45:10 +0200

    Updated validator to send Customer to status Active
    Not only that all linked projects must be finalized (Canceled, Decommissioned, or In Production), but at least one project must be in status In Production

--- Workflows/crm-customer-to-active-validator.js
+++ Workflows/crm-customer-to-active-validator.js
@@ -1,5 +1,9 @@
-// Validation message: All linked projects must be finalized before the customer can become active
+// Validation message: All linked projects must be finalized and at least one must be in production before the customer can become active
 issue.links
      .filter(link => link.type.name == "Customer-Project" &&
              !["Canceled", "In Production", "Decommissioned"].includes(link?.outwardIssue?.status?.name))
-     .length == 0
+     .length == 0 &&
+issue.links
+     .filter(link => link.type.name == "Customer-Project" &&
+             ["In Production"].includes(link?.outwardIssue?.status?.name))
+     .length > 0
