eb46a77055db4550de3776c073e5544d8bdc7c6e
Levente F <thebe14@gmail.com>
2025-08-31 12:58:04 +0200

    feature: Added deactivation attempt of Customer when Project canceled or decommissioned

--- Script Listeners/crm-project-canceled-or-decommissioned.js
+++ Script Listeners/crm-project-canceled-or-decommissioned.js
@@ -116,3 +116,70 @@ if(status.equals("Canceled")) {
         }
     }
 }
+
+if(status.equals("Canceled") || status.equals("Decommissioned")) {
+    // project canceled or decommissioned, deactivate customer(s)
+    def customers = []
+    def links = issue.fields.issuelinks as Map
+
+    for(def link : links)
+        if(link?.type.name.equals("Customer-Project") && null != link?.inwardIssue)
+            customers.add(link.inwardIssue)
+
+    // for all linked customers...
+    for(def customer : customers) {
+        // if we don't have the status of the customer, get it
+        if(null == customer.fields?.status) {
+            result = get("/rest/api/3/issue/${customer.key}")
+                .header("Content-Type", "application/json")
+                .asObject(Map)
+
+            if(result.status < 200 || result.status > 204) {
+                logger.info("Could not get custoemr ${customer.key} (${result.status})")
+                continue
+            }
+
+            customer = result.body
+        }
+
+        // if the customer is in status Active
+        if(!customer.fields?.status?.name.equals("Active")) {
+            // get the possible transitions on the customer
+            def transitions = [:]
+            result = get("/rest/api/3/issue/${customer.key}/transitions")
+                .header("Accept", "application/json")
+                .asObject(Map)
+
+            if(result.status < 200 || result.status > 204) {
+                logger.info("Could not get transitions of ${customer.key} (${result.status})")
+                continue
+            }
+
+            for(def transition in result.body?.transitions)
+                transitions[transition.name] = transition.id
+
+            def transName = "Deactivate"
+            def transId = transitions[transName]
+
+            if(null == transId) {
+                logger.info("${transName} transition not available on ${customer.key}")
+                continue
+            }
+
+            // transition customer to status Inactive
+            result = post("/rest/api/3/issue/${customer.key}/transitions")
+                .header("Content-Type", "application/json")
+                .body([
+                    transition: [
+                        id: transId,
+                    ]
+                ])
+                .asString()
+
+            if(result.status < 200 || result.status > 204)
+                logger.info("Could not transition customer ${customer.key} via ${transName} (${result.status})")
+
+            logger.info("Deactivated customer ${customer.key}")
+        }
+    }    
+}
