08fef867ad57fdeb8a6b231d06718affa96da4b3
Levente F <thebe14@gmail.com>
2025-09-20 12:14:23 +0200

    feat: Moved init of Customer Database Review to script listener so it happens even if you create ticket manually
    Init now brings in the guide from the Process CRM ticket and lists all high priority customers in field "Focus on customers"

--- Jira/Script Listeners/crm-database-review-init.js
+++ Jira/Script Listeners/crm-database-review-init.js
@@ -12,34 +12,50 @@ if(summary.toLowerCase().trim() == "test") {
 }
 
 /***
- * Fetch and return the correct Process ticket for the current Jira project
- * @param processCode is the SMS process code
+ * Fetch and return the tickets matching a specific JQL query
+ * @param jql is the JQL query to use to search
  * @param fieldsToFetch is array with Ids of the fields to return, or null to get everything
- * @returns Process ticket, null or error
+ * @param maxResults is the maximum number of tickets to return
+ * @returns Returns array of tickets (1K at most), null or error
  */
-def getProcess(processCode, fieldsToFetch) {
+def findTickets(jql, fieldsToFetch, maxResults = 1000) {
     // find the ticket of 
     def result = post("/rest/api/3/search/jql") 
             .header("Content-Type", "application/json")
             .header("Accept", "application/json")
             .body([
                 fields: null != fieldsToFetch ? fieldsToFetch : [ "*all" ],
-                jql: "project=SMS and issuetype='Process ${processCode}'",
-                maxResults: 1
+                jql: jql,
+                maxResults: maxResults
             ])
             .asObject(Map)
 
     if(result.status < 200 || result.status > 204) {
-        logger.info("Could not search for ${processCode} process ticket (${result.status})")
+        logger.info("Could not search for tickets (${result.status})")
         return null
     }
 
     if(null == result.body.issues || result.body.issues.isEmpty()) {
-        logger.info("Could not find ${processCode} process ticket (${result.status})")
-        return null
+        logger.info("Could not find tickets (${result.status})")
+        return []
     }
 
-    return result.body.issues[0]
+    return result.body.issues
+}
+
+/***
+ * Fetch and return the correct Process ticket for a Jira project
+ * @param processCode is the SMS process code
+ * @param fieldsToFetch is array with Ids of the fields to return, or null to get everything
+ * @returns Process ticket, null or error
+ */
+def getProcess(processCode, fieldsToFetch) {
+    // find the Process ticket of the specified SMS process
+    def tickets = findTickets("project=SMS and issuetype='Process ${processCode}'", fieldsToFetch, 1)
+    if(null == tickets || tickets.isEmpty())
+        return null;
+    
+    return tickets[0]
 }
 
 // get custom fields
@@ -51,6 +67,10 @@ def customFields = get("/rest/api/3/field")
 def processOwnerId = customFields.find { it.name == 'Process owner' }?.id?.toString()
 def processManagerId = customFields.find { it.name == 'Process manager' }?.id?.toString()
 def dbReviewGuideId = customFields.find { it.name == 'Database review guide' }?.id?.toString()
+def focusCustomersId = customFields.find { it.name == 'Focus on customers' }?.id?.toString()
+def customerNameId = customFields.find { it.name == 'Customer name' }?.id?.toString()
+def customerPriorityId = customFields.find { it.name == 'Customer priority' }?.id?.toString()
+def customerOwnerId = customFields.find { it.name == 'Customer owner' }?.id?.toString()
 
 // find and fetch the correct process ticket
 def process = getProcess("CRM", ["key", processOwnerId, processManagerId, dbReviewGuideId])
@@ -70,6 +90,60 @@ if(null == assignee) {
         assignee = [ accountId: processManager ]
 }
 
+// find high priority customers
+def focusCustomers = null
+def jql = "project=CRM and issuetype=Customer and 'Customer priority[Dropdown]' in ('Strategic community', 'Community with high number of users', 'Politically important growing community')"
+def customers = findTickets(jql, ["key", "summary", customerOwnerId, customerNameId, customerPriorityId])
+if(null != customers) {
+    focusCustomers = [
+        type: "doc",
+        version: 1,
+        content: [
+            [
+                type: "paragraph",
+                content: [[
+                    type: "text",
+                    text: "These are the most important customers:",
+                ]]
+            ],
+            [
+                type: "bulletList",
+                content: []
+            ]
+        ]
+    ]
+
+    for(def customer in customers) {
+        def customerName = customer.fields[customerNameId] as String
+        def customerPriority = customer.fields[customerPriorityId]?.value as String
+
+        def listItem = [
+            type: "listItem",
+            content: [
+                [
+                    type: "paragraph",
+                    content: [
+                        [
+                            type: "text",
+                            text: customerName,
+                            marks: [[
+                                type: "link",
+                                attrs: [ href: "/browse/${customer.key}" ]
+                            ]]
+                        ],
+                        [
+                            type: "text",
+                            text: " is a ${customerPriority.toLowerCase()}"
+                        ]
+                    ]
+                ]
+            ]
+        ]
+
+        focusCustomers.content[1].content.add(listItem)
+    }
+}
+
 // store the customer satisfaction review frequency on the customer ticket
 def result = put("/rest/api/3/issue/${issue.key}")
     .queryString("overrideScreenSecurity", Boolean.TRUE)
@@ -80,6 +154,7 @@ def result = put("/rest/api/3/issue/${issue.key}")
             (processOwnerId): [ accountId: processOwner ],
             (processManagerId): [ accountId: processManager ],
             description: dbReviewGuide,
+            (focusCustomersId): focusCustomers
         ],
     ])
     .asString()
